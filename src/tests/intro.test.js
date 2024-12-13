2fe79d8ae9c9db944a2810c6bc93543d0d0640ac
mahya.kabiri <mahya.kabiri@snapp.cab>
2024-12-13 13:00:34 +0330

    first commit

---
+++ src/tests/intro.test.js
@@ -0,0 +1,17 @@
+import { describe, test, it, expect } from "vitest";
+import { max } from "../intro";
+
+describe("max", () => {
+	//this is function which called by test runners
+	it("should return the first argument if it is greater", () => {
+		// AAA pattern => arrange(setup test env), act(perform the action), assert(check the out code);
+		// Arrange
+		const a = 2;
+		const b = 1;
+		//Act
+		const result = max(a, b);
+		//assert
+		expect(result).toBe(2);
+    // test in a single line => expect(max(2, 1)).toBe(2);`
+	});
+});
