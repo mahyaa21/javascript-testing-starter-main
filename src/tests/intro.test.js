d2b9b4f78f1d47e32a1b693dd9c920af3ed568e3
mahya.kabiri <mahya.kabiri@snapp.cab>
2024-12-13 13:14:10 +0330

    feat: add fizzbuzz test cases

--- src/tests/intro.test.js
+++ src/tests/intro.test.js
@@ -1,5 +1,5 @@
 import { describe, test, it, expect } from "vitest";
-import { max } from "../intro";
+import { fizzBuzz, max } from "../intro";
 
 describe("max", () => {
 	//this is function which called by test runners
@@ -12,14 +12,33 @@ describe("max", () => {
 		const result = max(a, b);
 		//assert
 		expect(result).toBe(2);
-    // test in a single line => expect(max(2, 1)).toBe(2);`
+		// test in a single line => expect(max(2, 1)).toBe(2);`
 	});
 
-  it("should return the second argument if it is greater", () => {
-    expect(max(1, 2)).toBe(2);
+	it("should return the second argument if it is greater", () => {
+		expect(max(1, 2)).toBe(2);
+	});
+
+	it("should return the first argument if arguments are equal", () => {
+		expect(max(1, 1)).toBe(1);
+	});
+});
+
+describe("FizzBuzz", () => {
+	it('should return "Fizz" if number is divisible by 3', () => {
+    expect(fizzBuzz(24)).toBe("Fizz");
   });
 
-  it("should return the first argument if arguments are equal", () => {
-    expect(max(1, 1)).toBe(1);
+	it("should return 'Buzz' if number is divisible by 5", () => {
+    expect(fizzBuzz(35)).toBe("Buzz");
+  });
+
+  it("should return 'FizzBuzz' if number is divisible by 3 and 5", () => {
+    expect(fizzBuzz(15)).toBe("FizzBuzz");
+  });
+
+  it("should return the number if it is not divisible by 3 or 5", () => {
+    expect(fizzBuzz(7)).toBe("7");
+    // .toBeDefined always show true even if production code and test code are different
   });
 });
